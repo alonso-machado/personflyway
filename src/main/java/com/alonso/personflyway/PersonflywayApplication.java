18ca9566af06f3c4d6de30acaa1065aa3cf96943
Alonso Machado <84375734+alonso-machado@users.noreply.github.com>
2023-11-28 10:45:08 -0400

    Thymeleaf add .html

--- src/main/java/com/alonso/personflyway/PersonflywayApplication.java
+++ src/main/java/com/alonso/personflyway/PersonflywayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages="com.alonso.personflyway.repository")
 @EnableTransactionManagement
+@EnableWebMvc
 public class PersonflywayApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/alonso/personflyway/controller/ThymeleafController.java
+++ src/main/java/com/alonso/personflyway/controller/ThymeleafController.java
@@ -10,12 +10,14 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/pages/")
+@Validated
+@RequestMapping("/pages")
 public class ThymeleafController {
 
 	private PersonService service;
@@ -26,14 +28,14 @@ public class ThymeleafController {
 	                         Model model) {
 		model.addAttribute("persons", service.findAll(page, size));
 
-		return "index";
+		return "index.html";
 	}
 
 	@GetMapping("/save/")
 	@Operation(summary = "Return Thymeleaf Page to Create that Person in the System.")
-	public String saveperson(Model model) {
+	public String savePersonPage(Model model) {
 		model.addAttribute("person", new PersonDTO());
-		return "saveperson";
+		return "saveperson.html";
 	}
 
 	@GetMapping("/edit/{id}")
@@ -41,7 +43,7 @@ public class ThymeleafController {
 	public String updateWholePersonPage(@NotNull @PathVariable Integer id, Model model) {
 		PersonDTO p = service.findById(id);
 		model.addAttribute("person", service.updatePerson(id, p));
-		return "showperson";
+		return "showperson.html";
 	}
 
 	@GetMapping("/delete/{id}")
@@ -49,6 +51,6 @@ public class ThymeleafController {
 	public String deletePersonPage(@NotNull @PathVariable Integer id, Model model) {
 		PersonDTO p = service.findById(id);
 		model.addAttribute("person", service.updatePerson(id, p));
-		return "deleteperson";
+		return "deleteperson.html";
 	}
 }
